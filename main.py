1763f7df8626c63c3ca97aa3e07c0fd792914c2f
Felicity Graham <Felicity.rose.graham@outlook.com>
2023-09-10 20:14:41 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello World')
+
+for x in range(3):
+    print('loops')
+
+for x in range(1,4):
+    print('poops')
+
+for x in range(2):
+    print('and scoops')
+else:
+    print("Almost done!")
+
+for x in range(3):
+     print('test')
+     print('testing')
+else:
+    print("test complete!")
